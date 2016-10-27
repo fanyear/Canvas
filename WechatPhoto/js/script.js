f7c85f781b63c68c631f53550dc9fecc5c8ebc54
fanyear <523585036@qq.com>
2016-10-28 00:40:54 +0800

    WechatPhoto Done

---
+++ WechatPhoto/js/script.js
@@ -0,0 +1,79 @@
+var canvasHeight = document.body.clientHeight;
+var canvasWidth = document.body.clientWidth;
+var radius = 50;
+
+var canvas = document.getElementById("blur-canvas");
+var context = canvas.getContext("2d")
+var resetButton = document.getElementById("reset");
+var showButton = document.getElementById("show");
+
+
+canvas.height = canvasHeight;
+canvas.width = canvasWidth;
+var div = document.getElementById("blur-div");
+div.style.width = canvas.width +"px";
+div.style.height = canvas.height +"px";
+
+var img = document.getElementById("blur-image");
+img.style.width = img.width+"px";
+img.style.height = img.height+"px";
+
+var leftMargin = parseInt((img.width - canvas.width)/2);
+var topMargin = parseInt((img.height - canvas.height)/2);
+img.style.left = String(-leftMargin)+"px";
+console.log(leftMargin);
+img.style.top = String(-topMargin)+"px";
+
+var clippingRegion = {
+    x: 200,
+    y: 300,
+    r: 50
+};
+
+var image = new Image();
+image.src = "js/img.jpg";
+image.onload = function() {
+
+
+    initCancas(clippingRegion);
+}
+
+function initCancas() {
+    draw(image, clippingRegion);
+}
+
+function setclippingRegion(clippingRegion) {
+    context.beginPath();
+    context.arc(clippingRegion.x, clippingRegion.y, clippingRegion.r, 0, 2 * Math.PI);
+    context.clip();
+}
+
+function draw(image, clippingRegion) {
+
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.save();
+    setclippingRegion(clippingRegion);
+    context.drawImage(image, Math.max(leftMargin,0),Math.max(topMargin,0),
+    Math.min(canvas.width,image.width),Math.min(canvas.height,image.height),
+    Math.max(-leftMargin,0), Math.max(-topMargin,0), Math.min(canvas.width,image.width),
+    Math.min(canvas.height,image.height));
+    context.restore();
+}
+
+resetButton.onclick = function() {
+
+    clippingRegion.x = Math.floor((canvas.width - 2 * radius - 2*Math.max(-leftMargin,0)) * Math.random()) + Math.max(-leftMargin,0)+radius;
+    clippingRegion.y = Math.floor((canvas.height - 2 * radius- 2*Math.max(-topMargin,0)) * Math.random()) + radius +Math.max(-topMargin,0);
+    clippingRegion.r = 50;
+    draw(image, clippingRegion);
+}
+showButton.onclick = function() {
+
+    var theAnimation = setInterval(function() {
+        clippingRegion.r += 40;
+        draw(image, clippingRegion);
+        if (clippingRegion.r >= Math.max(canvas.width, canvas.height)) {
+            clearInterval(theAnimation);
+        }
+    }, 30)
+}
---
+++ WechatPhoto/readme.md
@@ -0,0 +1,3 @@
+#### 链接：[https://fanyear.github.io/Canvas/WechatPhoto/index.html](https://fanyear.github.io/Canvas/WechatPhoto/index.html)
+
+#### 参考MOOC Canvas教程  [http://www.imooc.com/learn/601](http://www.imooc.com/learn/601)
